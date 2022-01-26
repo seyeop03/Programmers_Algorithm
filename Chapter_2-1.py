f7c0379f39574e73bd67167e9a4f41bfabdc4f06
seyeop <seyeop03@gmail.com>
2022-01-26 20:09:47 +0900

    Create Chapter_2-1.py and License

---
+++ Chapter_2-1.py
@@ -0,0 +1,29 @@
+### 정렬된 리스트에 원소 삽입
+
+# 전형적인 python 방식
+def solution(L, x):
+    for idx, num in enumerate(L):
+        if num > x:
+            idx -= 1
+            break
+    L.insert(idx+1,x)
+    
+    return L
+
+# 이진탐색 방식
+def solution(L, x):
+    first = 0
+    last = len(L)-1
+
+    while first <= last:
+        middle = (first+last) // 2
+        if L[middle] == x:
+            L.insert(middle, x)
+        elif L[middle] < x:
+            first = middle + 1
+        elif x < L[middle]:
+            last = middle - 1
+
+    L.insert(first, x)    
+    answer = L
+    return answer
