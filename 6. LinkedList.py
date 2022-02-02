f2197b840e46ccb7ade32b6dfdd15e75b89f50bc
seyeop <seyeop03@gmail.com>
2022-02-02 20:40:49 +0900

    Create 6. LinkedList.py

---
+++ 6. LinkedList.py
@@ -0,0 +1,89 @@
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = None
+        self.tail = None
+
+    def __repr__(self):
+        if self.nodeCount == 0:
+            return 'LinkedList: empty'
+        s = ''
+        curr = self.head
+        while curr is not None:
+            s += repr(curr.data)
+            if curr.next is not None:
+                s += ' -> '
+            curr = curr.next
+        return s
+
+    def getAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+
+        i = 1
+        curr = self.head
+        while i < pos:
+            curr = curr.next
+            i += 1
+
+        return curr
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        if pos == 1:
+            newNode.next = self.head
+            self.head = newNode
+
+        else:
+            if pos == self.nodeCount + 1:
+                prev = self.tail
+            else:
+                prev = self.getAt(pos - 1)
+            newNode.next = prev.next
+            prev.next = newNode
+
+        if pos == self.nodeCount + 1:
+            self.tail = newNode
+
+        self.nodeCount += 1
+        return True
+
+    def popAt(self, pos):
+            if pos < 1 or self.nodeCount < pos:
+                raise IndexError
+            if pos == 1:
+                curr = self.head
+                self.head = curr.next # 2번째 노드를 head로
+                if pos == self.nodeCount:
+                    self.tail = None
+            else:
+                prev = self.getAt(pos-1)
+                curr = prev.next
+                prev.next = curr.next
+                if pos == self.nodeCount:
+                    self.tail = prev
+            self.nodeCount -= 1
+            return curr.data
+
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr is not None:
+            result.append(curr.data)
+            curr = curr.next
+        return result
+
+
+def solution(x):
+    return 0
