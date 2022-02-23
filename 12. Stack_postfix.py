aa7c7ff76335b67876ff39cdcb5b2949ef74ae53
seyeop <seyeop03@gmail.com>
2022-02-23 20:52:07 +0900

    Make infix notation => postfix notation

---
+++ 12. Stack_postfix.py
@@ -0,0 +1,51 @@
+import string
+
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+prec = {
+    '*': 3, '/': 3,
+    '+': 2, '-': 2,
+    '(': 1
+}
+
+def solution(S):
+    opStack = ArrayStack()
+    answer = ''
+    
+    for c in S:
+        if c in string.ascii_uppercase:
+            answer += c
+        elif c == '(':
+            opStack.push(c)
+        elif c == ')':
+            while opStack.peek() != '(':
+                answer += opStack.pop()
+            opStack.pop()
+        elif c in prec:
+            if not opStack.isEmpty():
+                while not opStack.isEmpty() and prec[opStack.peek()] >= prec[c]:
+                    answer += opStack.pop()
+            opStack.push(c)      
+            
+    while not opStack.isEmpty():
+        answer += opStack.pop()
+    
+    return answer
