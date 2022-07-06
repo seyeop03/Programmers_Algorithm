29868c95b11cd2c357ee3a67dde1e95f887107d6
seyeop <seyeop03@gmail.com>
2022-07-06 16:51:42 +0900

    Modify 18.Binary_Tree(inorder).py

---
+++ 18. Binary_Tree(inorder).py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.left = None
+        self.right = None
+
+    ###################### 이진트리 노드의 총 갯수 ######################
+    # 전체 이진 트리의 size() ==  left subtree의 size() + right subtree의 size() + 1
+    # 또 <left subtree>, <right subtree> 의 각 root가 있을테니 각 노드의 left subtree의 size() + right subtree의 size() + 1
+    # ⋯ 계속 ⋯ (I'm gonna reduce it again and keep doing that until I get down to a simple case that I can solve directly.)
+    # <추가> : L에서 l+r+1을 계산해서오고, R에서도 l+r+1을 계산해서 온다면, 호출한 Node를 포함한 갯수가 L+R+1임은 자명하다.
+    def size(self):
+        l = self.left.size() if self.left else 0 # 지금 노드(n)의 왼쪽 자식이 있으면 left.size() 호출
+        r = self.right.size() if self.right else 0 # 지금 노드(n)의 오른쪽 자식이 있으면 right.size() 호출
+        return l + r + 1
+
+    # left subtree의 depth()와 right subtree의 depth()중 <더 큰 것 + 1>    
+    def depth(self):
+        l = self.left.depth() if self.left else 0
+        r = self.right.depth() if self.right else 0
+        if l > r:
+            return l + 1
+        else:
+            return r + 1
+
+    # Depth Breadth First의 중위순회(in-order)는 left->자기자신->right 이다.
+    def inorder(self):
+        traversal = []
+        if self.left: # left먼저
+            traversal += self.left.inorder()
+        traversal.append(self.data) # 그다음 자기자신 집어넣기
+        if self.right: # 마지막 right
+            traversal += self.right.inorder()
+        return traversal
+
+class BinaryTree:
+    # 트리의 루트노드 지정
+    def __init__(self, r):
+        self.root = r
+    
+    
+    def size(self):
+        if self.root:
+            return self.root.size()
+        else:
+            return 0
+
+    def depth(self):
+        if self.root:
+            return self.root.depth()
+        else:
+            return 0
+    
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return 0
--- 프로그래머스_크롤링.py
+++ 프로그래머스_크롤링.py
@@ -21,3 +21,37 @@ for i in range(18):
         if note_soup.select_one('#note') is not None:
             print(note_soup.select_one('#note'))
             print('#########################################################')
+================================================================================
+================================================================================
+================================================================================
+================================================================================
+================================================================================
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://programmers.co.kr/learn/courses/2'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+specific_lecture_list = soup.select_one(
+    '#curriculum > div > div > div[data-index="0"]')
+
+for i in range(5):
+    url_selector = 'ul > li:nth-child({}) > div.lesson-detail-wrapper > div.title-wrapper > a'.format(i+1)
+    note_url = specific_lecture_list.select_one(url_selector)['href']
+    print(note_url)
+    note_html = requests.get(note_url)
+    note_soup = BeautifulSoup(note_html.text, 'html.parser')
+    print(note_soup.select('#note'))
+
+
+""" selector
+#note > div.markdown.github
+
+body > div.main > div.lesson-content-bottom > div.content-tab > div > ul > li:nth-child(1) > a
+
+#curriculum > div > div > div:nth-child(1) > ul > li:nth-child(4) > div.lesson-detail-wrapper > div.title-wrapper > a
+#curriculum > div > div > div:nth-child(1) > ul > li:nth-child(5) > div.lesson-detail-wrapper > div.title-wrapper > a
+# ul > li:nth-child(4) > div.lesson-detail-wrapper > div.title-wrapper > a
+"""
